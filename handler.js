886076728a0509761f649411fe036360505dfc0e
pritam <pritamkhose@gmail.com>
2020-01-24 00:52:39 +0530

    update

---
+++ handler.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Welcome, AWS Serverless Lambda',
+        date: (new Date().toISOString()),
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.info = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
